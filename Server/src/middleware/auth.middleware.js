5a80772262029ad0cb3475817234ba284244a107
Ayan husain03 <hussainayan9897@gmail.com>
2026-01-25 20:19:43 +0530

    add authentication middleware for token verification and user validation

---
+++ Server/src/middleware/auth.middleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import FoodPartner from "../models/foodpartner.model.js";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.token || req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication failed. Token missing",
+      });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECERET);
+    const foodPartner = await FoodPartner.findById(
+      decoded._id || decoded.id,
+    ).select("-password");
+    if (!foodPartner) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication failed. Invalid token",
+      });
+    }
+    req.foodPartner = foodPartner;
+    next();
+  } catch (error) {
+    console.error("Auth Middleware Error:", error.message);
+    return res.status(401).json({
+      success: false,
+      message: "Authentication failed. something went wrong",
+    });
+  }
+};
+
+export default authMiddleware;
