a775faf302cb3bad8d0a39ea8007109d602a7767
Ayan husain03 <hussainayan9897@gmail.com>
2026-02-20 21:43:04 +0530

    feat: add root route to confirm backend is running successfully

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,9 @@ import app from "./src/app.js";
 import connectDb from "./src/db/db.js";
 
 connectDb();
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
 app.listen(3000, () => {
   console.log(`you server is running on 3000`);
 });
